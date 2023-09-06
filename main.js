8bd283e16b7b17b18044cffdd002293cf23d2e27
DinoLuke <112709256+DinoLuke@users.noreply.github.com>
2023-09-06 18:26:12 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -6,14 +6,13 @@ var VideoLegalzinhoAleatorio = "";
 
 function preload() {
 
+VideoLegalzinhoAleatorio = createVideo("video.mp4");
 }
 
 function setup() {
 
 canvas = createCanvas(480, 380);
 canvas.center();
-VideoLegalzinhoAleatorio = createVideo(VIDEO);
-VideoLegalzinhoAleatorio.size(480,380)
 VideoLegalzinhoAleatorio.hide();
 }
 
@@ -26,26 +25,13 @@ ObjectDetector.detect(VideoLegalzinhoAleatorio, gotResults);
 for(i=0; i< objetos.length; i++) {
 
 document.getElementById("STATUS").innerHTML = "Status: Objetos Detectados";
-//document.getElementById("NumeroObjeto").innerHTML = "quantidade de Objetos Detectados: " + objetos.length;
+document.getElementById("NumeroObjeto").innerHTML = "quantidade de Objetos Detectados: " + objetos.length;
 fill("purple");
 percent = floor(objetos[i].confidence*100);
 text(objetos[i].label+""+percent+"%",objetos[i].x+15,objetos[i].y+15);
 noFill();
 stroke("red");
 rect(objetos[i].x,objetos[i].y,objetos[i].width,objetos[i].height);
-if(objetos[i].label == objectName) {
-
-VideoLegalzinhoAleatorio.stop();
-objectDetector.detect(gotResults);
-document.getElementById("objectStatus").innerHTML = objectName + " Encontrado";
-synth = window.speechSynthesis;
-utterThis = new SpeechSynthesisUtterance(objectName + " Encontrado");
-synth.speak(utterThis);
-}
-else {
-
-document.getElementById("objectStatus").innerHTML = objectName + " Não Encontrado";
-}
 }
 }
 }
@@ -64,11 +50,13 @@ function START() {
 
 ObjectDetector = ml5.objectDetector("cocossd", modelload);
 document.getElementById("STATUS").innerHTML = "Status: Detectando Objetos";
-ObjectName = document.getElementById("objectName").value;
 }
 
 function modelload() {
 
 console.log("modelo aleatorio");
 statusModelo = true;
+VideoLegalzinhoAleatorio.loop();
+VideoLegalzinhoAleatorio.speed(1);
+VideoLegalzinhoAleatorio.volume(20);
 }
